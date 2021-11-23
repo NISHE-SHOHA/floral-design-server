ba96185d7c3b1bc65418a9ef91b86326a4aeb74b
NISHE-SHOHA <nazmakhatunnishe@gmail.com>
2021-11-23 16:46:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleWere
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mht4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('floralDesign');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders');
+
+
+        //GET ALL DATA
+        app.get('/services', async (req,res)=>{
+          const cursor = servicesCollection.find({});
+          const services = await cursor.toArray();
+          res.send(services);
+        })
+
+        //Order Get all data
+        app.get('/orders', async (req,res)=>{
+          const cursor = ordersCollection.find({});
+          const orders = await cursor.toArray();
+          res.send(orders);
+        })
+
+        //GET SINGLE DATA
+        app.get('/services/:id', async(req, res)=>{
+          const id = req.params.id;
+          console.log('getting specific service', id);
+          const query = { _id: ObjectId(id) };
+          const service = await servicesCollection.findOne(query);
+          res.json(service);
+        })
+
+
+        //POST API
+       app.post('/services', async(req,res) =>{
+         const service = req.body;
+        console.log('hit the post api', service)
+        const result = await servicesCollection.insertOne(service);
+        res.json(result)
+
+       });
+
+
+       //order post api
+       app.post('/orders', async(req,res) =>{
+        const order = req.body;
+        console.log(order)
+        const result = await ordersCollection.insertOne(order);
+        console.log(result)
+        res.json(result)
+
+      });
+       
+    //DELETE API
+    app.delete('/services/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id:ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    })
+
+      
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
